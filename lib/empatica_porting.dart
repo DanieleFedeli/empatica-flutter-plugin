f3918359c196bd7d38f7f473fe9447792063d6ad
Daniele Fedeli <d.fedeli@commongrounds.com>
2021-06-08 15:43:38 +0200

    Added barrel file

--- lib/empatica_porting.dart
+++ lib/empatica_porting.dart
@@ -1,14 +1,2 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class EmpaticaPorting {
-  static const MethodChannel _channel =
-      const MethodChannel('empatica_porting');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'bluetooth_device.dart';
+export 'empatica_manager.dart';
